987e16b230bc7e987f24581c1c617e6628205152
UmayerK <95543241+UmayerK@users.noreply.github.com>
2023-10-03 22:43:21 +0000

    hahaha

--- src/source.asm
+++ src/source.asm
@@ -1,8 +1,8 @@
 TITLE
 
-; Name: Faraan Rashid
+; Name: umayer khan 
 ; Date: 
-; ID: 
+; ID: 110105667
 ; Description: 
 
 INCLUDE Irvine32.inc
@@ -12,90 +12,63 @@ INCLUDELIB Irvine32.lib
 INCLUDELIB kernel32.lib
 INCLUDELIB user32.lib
 
+
 .data
-    Rval SDWORD 22
-    Xval SDWORD 26
-    Yval SDWORD 30
-    Zval SDWORD 40
+val1 BYTE 10h
+val2 WORD 8000h
+val3 DWORD 0FFFFh
+val4 WORD 7FFFh
+val5 DWORD ?
+arrayW WORD 100h,200h,300h
+byte 20 DUP(0)
 
 .code
 main PROC
-    mov ax,1000h
-    inc ax
-    dec ax
-    mov eax,Xval
-    neg eax
-    mov ebx,Yval
-    sub ebx,Zval
-    add eax,ebx
-    mov Rval,eax
-    mov cx,1
-    sub cx,1
-    mov ax,0FFFFh
-    inc ax
-    mov ecx,0B9F6h
-    sub ecx,9874h
+	
 
-    call DumpRegs
-    ; end of A
-    ; Carry Flag = 0
-    ; Overflow Flag = 0
-    ; Sign Flag = 0
-    ; Zero Flag = 0
-    ; CX = 0000
-    ; AL = 00
+;a increment val 12
+mov ax, val2
+inc ax;
+;b subtract
+sub eax, val3
+;c sub val 4 from val 2
+mov val2, ax
+sub val4, ax
+;d val5 = -val1 - (val2 + val4).
+mov al, val1
+neg al
+movsx eax, ax      ; Sign-extend AL to EAX (since it's signed)
+mov ax, val2       ; Load val2 into AX
+add ax, val4       ; Add val4 to AX
+neg ax             ; Negate AX (two's complement)
+add eax, eax       ; Multiply AX by 2 (since we negated it)
+add val5, eax      ; Store the result in val5
+;e the value of carry flag is 0 value of sign flag is 1
+add val2, 1
+call DumpRegs
+;f he value of carry flag is 0 value of sign flag is 1
+sub val4, 1
+call DumpRegs
+mov dx, [arrayW +4]
+add val4, dx 
 
-    mov cx,0
-    sub cx,1
-    mov ax,7FFFh
-    add ax,2
+     mov val2,7FF0h
 
-    call DumpRegs
-    ; end of B
-    ; Carry Flag = 0
-    ; Overflow Flag = 1 Generated as adding the numbers 2 and 7FFF causes overflow as its 16 digits  and exceeds 32 thousand and ax is 64 thousand as its already full(after subtracting 1 ax register)
-    ; Sign Flag = 1  Generated because the number  0 -1 is negative in cx register
-    ; Zero Flag = 0
-    ; CX = 0000
-    ; AL = 01
+     add val2,10h ; a. CF = 0 SF =1 ZF = 0 OF =1
+     call DumpRegs
+     add val2,1 ; b. CF = 0 SF = 1 ZF = 0 OF =0
+     call DumpRegs
+     add val2,2 ; c. CF = 0 SF = 1 ZF =0 OF =0
+     call DumpRegs
 
-    mov al,0FFh
-    add al,1
-    mov al,+127
-    add al,1
-    mov ax,7FFEh
-    add ax,22h
+     ; the first step to this is moving  val 1 to eax register 
+     ; add val 2 to eax 
+     ; sub val 3 from eax
+     ; move the final value to eax which is (val 3 - (val2+val1))
+	; code goes here
+	call DumpRegs ; displays registers in console
 
-    call DumpRegs
-    ; end of C
-    ; Carry Flag = 0
-    ; Overflow Flag = 1 Generated as adding the numbers  7FFEh + 22h to an already overflown number causes overflow as al is 8 bits and the FFFF plus anything is greater than what 8 bits can handle
-    ; Sign Flag = 1 sf stays on as the most significant digit is 1
-    ; Zero Flag = 0
-    ; CX = 0000
-    ; AL = 20
+	exit
 
-    mov al,-128
-    sub al,1
-    mov al,0DFh
-    add al,32h
-    mov al,72h
-    sub al,0E6h
-    mov bl,-127
-    dec bl
-
-    call DumpRegs
-    ; end of D
-    ; Carry Flag = 1 this occurs as there is a carry done when adding 72h and 32h This is as subtracting 128 and 1 does not make it so the number is small enough to overflow 
-    ; Overflow Flag = 0
-    ; Sign Flag = 1 Generated because the number is negative -127 (bl register)
-    ; Zero Flag = 0
-    ; CX = 0000
-    ; AL = 8C
-;The final value for ECX=0000
-;The final value of AL  = 8C
-
-    exit
 main ENDP
 END main
-﻿
